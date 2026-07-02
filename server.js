61b46f4d41eb4946c08e251fa536b8055b7d7fe0
spongano10-spec <spongano10@gmail.com>
2026-07-02 01:15:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.post('/v1/chat/completions', async (req, res) => {
       model: nimModel,
       messages: messages,
       temperature: temperature || 0.6,
-      max_tokens: max_tokens || 9024,
+      max_tokens: max_tokens || 4096,
       stream: stream || true
     };
 
